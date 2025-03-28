e938cc4d833cc92a6803bb9c397a18d301463b3d
petergsme <garciasan@esat-alumni.com>
2025-03-28 20:00:48 +0100

    update-website-class-28-03

---
+++ Clase8-BotonesCSS/Modales/modal.js
@@ -0,0 +1,13 @@
+// BOTON HACE QUE SALGA EL MODAL
+
+const buttonToOpenModal = document.querySelector("#modales .btn");
+const buttonToCloseModal = document.querySelector("#modal .btn--secondary");
+const modalSection = document.querySelector("#modal");
+
+buttonToOpenModal.addEventListener("click", (event) => {
+  modalSection.classList.toggle("modal-hidden");
+});
+
+buttonToCloseModal.addEventListener("click", (event) => {
+  modalSection.classList.toggle("modal-hidden");
+});
